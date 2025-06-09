061dbe4a472fc105a5e99f4050c2dab5544dbf80
Redes-19 <kevingonzalezr433@gmail.com>
2025-06-09 10:14:20 -0300

    intento de conectar supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const env = process.env.DB_PASSWORD
+console.log(env)
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'mfxpaasshtxlsxjxcyvr.supabase.co',
+      port: 5432,
+      username: 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: 'postgres',
+      synchronize: false,
+      ssl: {
+        rejectUnauthorized: false
+      }.
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
