382cad1976d2530a53840436b61ca771f8f9c063
Redes-19 <kevingonzalezr433@gmail.com>
2025-06-09 11:46:54 -0300

    hacemos ssl

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as https from 'https';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+ 
+ const httpsOptions = {
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert'),
+ }
+ 
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
