12a5d93e12bdfb7d9bd3e8518fc70ea03be11a38
Maxi <90803184+scriniariii@users.noreply.github.com>
2025-06-24 16:08:32 -0300

    Update ViajesTech DB.sql
    base de datos renombrada

--- ViajesTech DB.sql
+++ ViajesTech DB.sql
@@ -14,8 +14,8 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- Crear esquema
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8;
-USE `mydb`;
+CREATE SCHEMA IF NOT EXISTS `amelieSQL` DEFAULT CHARACTER SET utf8;
+USE `amelieSQL`;
 
 -- Tabla Hotel
 CREATE TABLE IF NOT EXISTS `Hotel` (
