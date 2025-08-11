30d746e32bc2d8312ecace8580b50bc9304ebea2
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2025-08-11 10:34:22 -0300

    Metatags finalizadas
    Termine de poner las metatags

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import IntroSection from "@/components/IntroSection";
-import Navbar from "@/components/Navbar";
 import PlacesSection from "@/components/PlacesSection";
-import Image from "next/image";
 
 export default function Home() {
-  return (
+  return (    
     <div className="min-h-screen bg-background">
-      <main className="max-w-7xl mx-auto">
-        <HeroSection />
-        <IntroSection />
-        <PlacesSection />
-      </main>
+    <main className="max-w-7xl mx-auto">
+      <HeroSection />
+      <IntroSection />
+      <PlacesSection />
+    </main>
     </div>
   );
 }
