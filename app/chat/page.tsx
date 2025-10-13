fc1fab7b873e37a92b2f503050762a07d4109bdd
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2025-10-13 08:51:36 -0300

    Proteccion ruta chat completa

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { ChatInterface, ChatInterfaceProps } from "@/components/ChatInterface"
 import { Router, useRouter } from "next/router";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-export default withAuthenticationRequired(function Chat(){
+export default withPageAuthRequired(function Chat(){
     
     const handleBack = () => {
     const router = useRouter()
