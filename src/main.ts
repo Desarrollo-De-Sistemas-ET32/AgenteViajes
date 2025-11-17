fd1aeebe2c899fafde343bb9f75897704043b050
Román Abalos <romanishida2602@gmail.com>
2025-11-17 12:46:44 -0300

    Se conecta Front con Backend
    Se conectan ambos lugares para que funciones correctamente.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  // configurar cors
-  app.enableCors();
+  const app = await NestFactory.create(AppModule); // <-- sin cors:true
+
+  app.enableCors({
+    origin: ['http://localhost:3001'], // solo tu frontend
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Accept',
+      'Origin',
+    ],
+    exposedHeaders: [
+      'Content-Range',
+      'X-Content-Range',
+      'X-Total-Count',
+    ], // Headers que el frontend puede leer en la respuesta
+    maxAge: 3600,
+  });
   
   
   app.use('/file/upload', (req, res, next) => {
@@ -13,7 +29,7 @@ async function bootstrap() {
     next();
   });
 
-  console.log('Server running on http://localhost:3000');
+  console.log('Server running on http://localhost:3307');
   console.log('Upload endpoint: http://localhost:3000/file/upload');
   
   await app.listen(3000);
